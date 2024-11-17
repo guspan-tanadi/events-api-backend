340edc58cc784b6ea650f6ea64597d8c8fc4be96
guspan-tanadi <guspan.tanadi@gmail.com>
2024-11-17 14:32:50 +0700

    chore: path full upload folder multer

--- src/middlewares/upload.middleware.ts
+++ src/middlewares/upload.middleware.ts
@@ -2,7 +2,7 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/");
+    cb(null, "https://raw.githubusercontent.com/guspan-tanadi/events-api-backend/refs/heads/master/uploads/");
   },
   filename: (req, file, cb) => {/*
     cb(null, `${Date.now()} - ${file.originalname}`);*/
