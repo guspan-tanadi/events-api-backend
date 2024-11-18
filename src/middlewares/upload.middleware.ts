d33a4737239f7cb50a99ffeffbad02ae0479d20e
guspan-tanadi <guspan.tanadi@gmail.com>
2024-11-18 08:53:13 +0700

    chore: path directory name

--- src/middlewares/upload.middleware.ts
+++ src/middlewares/upload.middleware.ts
@@ -1,8 +1,8 @@
 import multer from "multer";
-
+import path from "path";
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/");
+    cb(null, path.join(__dirname, "uploads/"));
   },
   filename: (req, file, cb) => {
     cb(null, `${Date.now()} - ${file.originalname}`);
